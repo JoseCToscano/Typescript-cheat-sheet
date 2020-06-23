f3891ffa795a3e04df2d8669e589951bf060a589
Jose Toscano <josetoscano@atratopago.com>
2020-06-23 18:07:54 -0500

    f

---
+++ maps/src/User.ts
@@ -0,0 +1,22 @@
+import faker from "faker";
+import { Mappable } from "./CustomMap";
+
+export class User implements Mappable {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  color: "blue";
+  markerContent(): string {
+    return `User Name: is ${this.name}`;
+  }
+
+  constructor() {
+    this.name = faker.name.findName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
